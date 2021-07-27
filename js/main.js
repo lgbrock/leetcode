25f445f2cdadd696481e2c83a208043b7b48cf55
Logan Brock <logan.g.brock@gmail.com>
2021-07-27 09:47:59 -0400

    Two Sum

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 // Two Sum
+const twoSum = (nums, target) => {
+	for (let i = 0; i < nums.length; i++) {
+		for (let j = i + 1; j < nums.length; j++) {
+			if (nums[i] + nums[j] === target) {
+				return [i, j];
+			}
+		}
+	}
+};
