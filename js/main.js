d34be4734b6372dfb2697bd2e8d9afe93d915b9d
Logan Brock <logan.g.brock@gmail.com>
2021-07-30 11:52:19 -0400

    First Unique Character in a String

--- js/main.js
+++ js/main.js
@@ -26,3 +26,12 @@ const isPalindrome = (x) =>
 
 // Reverse String
 const reverseString = (s) => s.reverse();
+
+// First Unique Character in a String
+const firstUniqChar = (s) => {
+	for (let i = 0; i < s.length; i++) {
+		if (s.indexOf(s[i]) == s.lastIndexOf(s[i])) return i;
+	}
+
+	return -1;
+};
