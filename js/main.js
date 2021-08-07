1b07812af247082aff92a387e8b186b9f20263d2
Logan Brock <logan.g.brock@gmail.com>
2021-08-07 12:15:42 -0400

    Roman to Integer

--- js/main.js
+++ js/main.js
@@ -89,3 +89,14 @@ const isPowerOfThree = (n) => {
 }
 
 const isPowerOfThree = (n) => (Math.log(n) / Math.log(3)).toFixed(10) % 1 === 0
+
+// Roman to Integer
+const romanToInt = (s) => {
+	const roman = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+	let result = 0;
+	for (let i = 0; i < s.length; i++) {
+		if (i > 0 && roman[s[i]] > roman[s[i - 1]]) result += roman[s[i]] - 2 * roman[s[i - 1]];
+		else result += roman[s[i]];
+	}
+	return result;
+}
