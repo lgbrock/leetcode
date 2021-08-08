3770c2e1b0ba8e28bb28022b27ff63062fb04ab1
Logan Brock <logan.g.brock@gmail.com>
2021-08-08 11:51:53 -0400

    Best Time to Buy and Sell Stock II

--- js/main.js
+++ js/main.js
@@ -100,3 +100,12 @@ const romanToInt = (s) => {
 	}
 	return result;
 }
+
+// Best Time to Buy and Sell Stock II
+const maxProfit = (prices) => {
+	let max = 0;
+	for (let i = 1; i < prices.length; i++) {
+		if (prices[i] - prices[i - 1] > 0) max += prices[i] - prices[i - 1];
+	}
+	return max;
+}
