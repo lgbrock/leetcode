e279cb39a6660d82d111ed953ed831390221ad41
Logan Brock <logan.g.brock@gmail.com>
2021-08-12 08:18:15 -0400

    First Bad Version

--- js/main.js
+++ js/main.js
@@ -129,12 +129,18 @@ const countPrimes = (n) => {
 };
 
 // First Bad Version
-const firstBadVersion = (n) => {
-	let left = 1, right = n;
-	while (left < right) {
-		let mid = (left + right) >> 1;
-		if (isBadVersion(mid)) right = mid;
-		else left = mid + 1;
-	}
-	return left;
-}
+const solution = (isBadVersion) => {
+	return function (n) {
+		let low = 1,
+			high = n;
+		while (low < high) {
+			let mid = low + Math.floor((high - low) / 2);
+			if (isBadVersion(mid)) {
+				high = mid;
+			} else {
+				low = mid + 1;
+			}
+		}
+		return low;
+	};
+};
