d1d2c4d1e02bf6b9530ea78d83c222ce379270e2
Logan Brock <logan.g.brock@gmail.com>
2021-08-13 09:11:46 -0400

    Implement strStr()

--- js/main.js
+++ js/main.js
@@ -110,7 +110,7 @@ const maxProfit = (prices) => {
 	}
 	return max;
 };
-*/
+
 
 // Count Primes
 const countPrimes = (n) => {
@@ -144,3 +144,20 @@ const solution = (isBadVersion) => {
 		return low;
 	};
 };
+*/
+
+// Implement strStr()
+const strStr = (haystack, needle) => {
+		for (let i = 0; i < haystack.length - needle.length + 1; i++) {
+		let found = true;
+		for (let j = 0; j < needle.length; j++) {
+			if (haystack[i + j] !== needle[j]) {
+				found = false;
+				break;
+			}
+		}
+		if (found) return i;
+	}
+	return -1;
+}
+	
