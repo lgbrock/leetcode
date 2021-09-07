8495496b10785da6302a356b4976469771a10ffd
Logan Brock <logan.g.brock@gmail.com>
2021-09-07 10:34:02 -0400

    Squares of a Sorted Array

--- js/main.js
+++ js/main.js
@@ -231,3 +231,12 @@ const findNumbers = (n) => {
 	}
 	return count;
 }
+
+// Squares of a Sorted Array
+const sortedSquares = (nums) => {
+	let result = [];
+	for (let i = 0; i < nums.length; i++) {
+		result.push(nums[i] * nums[i]);
+	}
+	return result.sort((a, b) => a - b);
+}
