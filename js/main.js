e4c3c7f661473058f181a3b0a972a01954e81f7d
Logan Brock <logan.g.brock@gmail.com>
2021-09-20 09:58:31 -0400

    Remove Duplicates from Sorted Array

--- js/main.js
+++ js/main.js
@@ -309,3 +309,14 @@ const replaceElements = (arr) => {
 	result.push(-1);
 	return result;
 };
+
+// Remove Duplicates from Sorted Array
+const removeDuplicates = (nums) => {
+	for (let i = 0; i < nums.length; i++) {
+		if (nums[i] === nums[i + 1]) {
+			nums.splice(i, 1);
+			i--;
+		}
+	}
+	return nums.length;
+};
