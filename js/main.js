4f53bc40ab6d5d17083135f3daf4de7084896ba9
Logan Brock <logan.g.brock@gmail.com>
2021-09-21 09:25:32 -0400

    Sort Array By Parity

--- js/main.js
+++ js/main.js
@@ -320,3 +320,14 @@ const removeDuplicates = (nums) => {
 	}
 	return nums.length;
 };
+
+// Sort Array By Parity
+const sortArrayByParity = (arr) => {
+	let even = [],
+		odd = [];
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] % 2 === 0) even.push(arr[i]);
+		else odd.push(arr[i]);
+	}
+	return even.concat(odd);
+}
