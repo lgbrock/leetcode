6589bbd5d28ad6583f95cdd87681200d05122822
Logan Brock <logan.g.brock@gmail.com>
2021-09-22 08:52:54 -0400

    Height Checker

--- js/main.js
+++ js/main.js
@@ -330,4 +330,16 @@ const sortArrayByParity = (arr) => {
 		else odd.push(arr[i]);
 	}
 	return even.concat(odd);
-}
+};
+
+// Height Checker
+const heightChecker = (heights) => {
+	const sorted = heights.slice().sort((a, b) => a - b);
+	let count = 0;
+	for (let i = 0; i < heights.length; i++) {
+		if (heights[i] !== sorted[i]) {
+			count++;
+		}
+	}
+	return count;
+};
