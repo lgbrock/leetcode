47d7d62e3df33cfc29157f4c11e0ad25a41f3b0f
Logan Brock <logan.g.brock@gmail.com>
2021-09-27 10:07:19 -0400

    Intersection of Two Arrays II

--- js/main.js
+++ js/main.js
@@ -376,3 +376,22 @@ const rotate = (nums, k) => {
 	}
 	return nums;
 };
+
+// Intersection of Two Arrays II
+const intersect = (nums1, nums2) => {
+	const map = {};
+	const result = [];
+
+	for (let i = 0; i < nums1.length; i++) {
+		map[nums1[i]] = (map[nums1[i]] || 0) + 1;
+	}
+
+	for (let i = 0; i < nums2.length; i++) {
+		if (map[nums2[i]]) {
+			result.push(nums2[i]);
+			map[nums2[i]]--;
+		}
+	}
+
+	return result;
+}
